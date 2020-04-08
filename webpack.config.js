8ca6ddead7b08c219d60632ab5f2b4e6a0987f02
chengtianqing <tianqingcheng@clubfactory.com>
2020-04-08 14:26:48 +0800

    feat 初始化项目

---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'src/index.html'),
+  filename: './index.html'
+})
+
+module.exports = {
+
+  entry: {
+    app: path.join(__dirname, 'src', 'index.tsx')
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      }
+    ]
+  },
+
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  plugins: [
+    htmlPlugin,
+  ],
+
+  devServer: {
+    open: true,
+  },
+  
+};
